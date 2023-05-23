04be6cf44b2d73e653163db0cd30f23118b25191
Syed Anas Ali <syed.anas.ali@sap.com>
2023-05-24 00:15:14 +0530

    Update config.go

--- collector/receiver/trace-receiver/tailtracer/config.go
+++ collector/receiver/trace-receiver/tailtracer/config.go
@@ -1,16 +1,7 @@
 package tailtracer
 
-import (
-	"fmt"
-	"time"
-
-)
-
 // Config represents the receiver config settings within the collector's config.yaml
 type Config struct {
-   config.ReceiverSettings `mapstructure:",squash"`
-   Interval       string `mapstructure:"interval"`
-   NumberOfTraces int `mapstructure:"number_of_traces"`
+	Interval       string `mapstructure:"interval"`
+	NumberOfTraces int    `mapstructure:"number_of_traces"`
 }
-
-
