1b7e0db98f47bd5d81c6a32f92f4c3e7a6c40e61
Syed Anas Ali <46435477+FirstOrangutan@users.noreply.github.com>
2023-05-23 23:24:06 +0530

    Update config.go

--- collector/receiver/trace-receiver/tailtracer/config.go
+++ collector/receiver/trace-receiver/tailtracer/config.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 
-	"go.opentelemetry.io/collector/config"
 )
 
 // Config represents the receiver config settings within the collector's config.yaml
@@ -15,16 +14,3 @@ type Config struct {
 }
 
 
-// Validate checks if the receiver configuration is valid
-func (cfg *Config) Validate() error {
-	if (cfg.NumberOfTraces < 1){
-	   return fmt.Errorf("number_of_traces must be at least 1")
-	}
-
-    interval, _ := time.ParseDuration(cfg.Interval)
-	if (interval.Minutes() < 1){
-		return fmt.Errorf("when defined, the interval has to be set to at least 1 minute")
-	 }
- 
-	return nil
- }
